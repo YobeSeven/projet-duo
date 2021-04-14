0909bdbc43721b43bb442525f215a0c4ca2a1a63
dawiteu <dawid@tararuj.be>
2021-04-14 17:11:37 +0200

    terrain ok et icones ok

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -8,11 +8,35 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{env("APP_NAME")}}</title>
     
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes"> 
+
+    <!-- Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <!-- Favicon
+    ================================================== -->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon.png">
+
+
+
     {{-- LINK CSS --}}
     <link rel="stylesheet" href={{asset('css/bootstrap.min.css')}}>
     <link rel="stylesheet" href={{asset('css/style.css')}}>
     <link rel="stylesheet" href={{asset('css/responsive.css')}}>
     <link rel="stylesheet" href={{asset('css/colors.css')}}>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
 </head>
 
 <body>
